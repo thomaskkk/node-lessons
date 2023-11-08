ef45ab13f61124dea5fe5f30886f742beef6d53e
Thomas Kuryura <thomas.adm@gmail.com>
2023-11-08 16:48:07 -0300

    Complete lesson 5.5

--- 5.5 REST APIs/index.js
+++ 5.5 REST APIs/index.js
@@ -12,7 +12,7 @@ const API_URL = "https://secrets-api.appbrewery.com";
 // https://secrets-api.appbrewery.com/
 
 //TODO 1: Add your own bearer token from the previous lesson.
-const yourBearerToken = "";
+const yourBearerToken = "8f6b4daa-dd68-47bd-b1b6-1fa796452b07";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
@@ -35,21 +35,69 @@ app.post("/get-secret", async (req, res) => {
 
 app.post("/post-secret", async (req, res) => {
   // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
+  const body = {
+    secret: req.body.secret,
+    score: req.body.score,
+  };
+  try {
+    const result = await axios.post(API_URL + "/secrets", body, config);
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
 });
 
 app.post("/put-secret", async (req, res) => {
   const searchId = req.body.id;
   // TODO 3: Use axios to PUT the data from req.body to the secrets api servers.
+  const body = {
+    secret: req.body.secret,
+    score: req.body.score,
+  };
+  try {
+    console.log(API_URL + "/secrets/" + searchId);
+    const result = await axios.put(
+      API_URL + "/secrets/" + searchId,
+      body,
+      config
+    );
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
 });
 
 app.post("/patch-secret", async (req, res) => {
   const searchId = req.body.id;
   // TODO 4: Use axios to PATCH the data from req.body to the secrets api servers.
+  var body = {};
+  if (req.body.secret) {
+    body.secret = req.body.secret;
+  }
+  if (req.body.score) {
+    body.score = req.body.score;
+  }
+  try {
+    const result = await axios.patch(
+      API_URL + "/secrets/" + searchId,
+      body,
+      config
+    );
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
 });
 
 app.post("/delete-secret", async (req, res) => {
   const searchId = req.body.id;
   // TODO 5: Use axios to DELETE the item with searchId from the secrets api servers.
+  try {
+    const result = await axios.delete(API_URL + "/secrets/" + searchId, config);
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
 });
 
 app.listen(port, () => {
--- 5.5 REST APIs/solution.js
+++ 5.5 REST APIs/solution.js
@@ -7,7 +7,7 @@ const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
 //Add your own bearer token from the previous lesson.
-const yourBearerToken = "08f3026d-9c6c-4d88-a3b2-c579dc106247";
+const yourBearerToken = "8f6b4daa-dd68-47bd-b1b6-1fa796452b07";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
