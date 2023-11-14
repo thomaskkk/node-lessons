e1f4aa201ed219dbd73e649e5deb2c265813b487
Thomas Kuryura <thomas.adm@gmail.com>
2023-11-14 14:20:48 -0300

    Secrets - Username and Password

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -1,11 +1,64 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import "dotenv/config";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URL);
+
+const userSchema = {
+  email: String,
+  password: String,
+};
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+
+  newUser
+    .save()
+    .then(res.render("secrets.ejs"))
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username, password: password })
+    .then((foundUser) => {
+      console.log(foundUser);
+      if (foundUser) {
+        res.render("secrets.ejs");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.listen(port, function () {
   console.log(`Server running on port: ${port}`);
